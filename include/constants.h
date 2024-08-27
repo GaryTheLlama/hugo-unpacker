ea4700b50b4a850d7bce48201d7bd8994b3c5d18
GaryTheLlama <garythellama@gmail.com>
2024-08-27 10:03:42 -0700

    Add frame rate config values

--- include/constants.h
+++ include/constants.h
@@ -3,7 +3,7 @@
 #include "data.h"
 
 #define TITLE "Hugo Unpacker"
-#define VERSION 0.2
+#define VERSION 0.3
 
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 400
@@ -16,6 +16,10 @@
 #define CONCAT(x, y) x ## y
 
 #define FULL_PATH_SIZE 256
+#define MAX_FILES_TO_SEARCH 1024
 
 #define COLOR_WHITE 0xFFFFFFFF
 #define COLOR_BLACK 0x00000000
+
+#define TARGET_FPS 30
+#define TARGET_FRAME_TIME (1000 / TARGET_FPS)
